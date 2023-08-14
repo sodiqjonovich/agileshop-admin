9306c49affd1686a4ffffcbd380f03505b0b40a7
O'tkirbek <O'tkirbek@DESKTOP-Q53DFVB>
2023-08-14 12:13:23 +0500

    dark mode and light mode is solved

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+   import IconDashboard from '../icons/IconDashboard.vue';
+   import IconCategories from "../icons/IconCategories.vue"
+   import IconCompanies from "../icons/IconCompanies.vue";
+   import IconProducts from "../icons/IconProducts.vue";
+   import IconUsers from "../icons/IconUsers.vue";
+   import IconOrders from "../icons/IconOrders.vue";
+   import IconSettings from "../icons/IconSettings.vue"
+</script>
+<template>
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+   <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+      <ul class="space-y-2 font-medium">
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <IconDashboard></IconDashboard>
+               <span class="ml-3">Dashboard</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <IconCategories></IconCategories>
+               <span class="flex-1 ml-3 whitespace-nowrap">Categories</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <IconCompanies></IconCompanies>
+               <span class="flex-1 ml-3 whitespace-nowrap">Companies</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <IconProducts></IconProducts>               
+               <span class="flex-1 ml-3 whitespace-nowrap">Products</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <IconUsers></IconUsers>
+               <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <IconOrders></IconOrders>
+               <span class="flex-1 ml-3 whitespace-nowrap">Orders</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <IconSettings></IconSettings>
+               <span class="flex-1 ml-3 whitespace-nowrap">Settings</span>
+            </a>
+         </li>
+      </ul>
+   </div>
+</aside>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from "../components/common/Header.vue";
+import Sidebar from "../components/common/Sidebar.vue";
+import Footer from "../components/common/Footer.vue"
+import ThemeSetter from "@/components/common/ThemeSetter.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ThemeSetter></ThemeSetter>
+  <Header></Header>
+  <Sidebar></Sidebar>
+  <div class="p-4 sm:ml-64">
+    <div class="p-4 mt-14">
+      <main>
+      </main>
+      <Footer></Footer>
+    </div>
+  </div>
 </template>
